447810c54bf5bdfc98c9b49c6f8f1cbf124a13f8
MilanSterkens <r0840939@student.thomasmore.be>
2022-05-25 06:52:17 +0000

    add scriptjes Viktor

--- docker/script_ftp.sh
+++ docker/script_ftp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-WEBID="web30"
+WEBID="web2515"
 USERNAME="banaan"
 PASSWORD="banaan123"
 
 echo "useraccount for  $USERNAME is being created.
 Please wait..."
 
-sudo useradd $USERNAME -p $PASSWORD -m -d /home/ubuntu/projecthosting/docker/banaan/www/
+
+sudo useradd $USERNAME -p $PASSWORD -d /home/ubuntu/projecthosting/docker/$WEBID/www/
+sudo chown -R $USERNAME /home/ubuntu/projecthosting/docker/$WEBID/www  
