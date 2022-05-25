73d9aa2c93e1869fc76f05fed41a7d70ae3f24fe
MilanSterkens <r0840939@student.thomasmore.be>
2022-05-25 06:54:31 +0000

    changes to ftp script

--- docker/script_ftp.sh
+++ docker/script_ftp.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-WEBID="web2515"
-USERNAME="banaan"
-PASSWORD="banaan123"
+WEBID="EEEEEEEEEEEEE"
+SFTP_USERNAME="jordy"
+SFTP_PASSWORD="jordy"
 
-echo "useraccount for  $USERNAME is being created.
+echo "useraccount for  $SFTP_USERNAME is being created.
 Please wait..."
 
+PASS=$(perl -e 'print crypt($ARGV[0], "password")' $SFTP_PASSWORD)
 
-sudo useradd $USERNAME -p $PASSWORD -d /home/ubuntu/projecthosting/docker/$WEBID/www/
-sudo chown -R $USERNAME /home/ubuntu/projecthosting/docker/$WEBID/www  
+#in future, this line can be deleted
+mkdir /home/ubuntu/projecthosting/docker/$WEBID
+sudo useradd $SFTP_USERNAME -p $PASS -m -d /home/ubuntu/projecthosting/docker/$WEBID/www
+
+sudo chown -R $SFTP_USERNAME /home/ubuntu/projecthosting/docker/$WEBID/www  
+
+
+echo $SFTP_USERNAME >> /etc/vsftpd.userlist
+
+echo "finished"
